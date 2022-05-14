79a86d8db670077cc2d2e12e166d16d87dc512c5
MJ-0701 <jack2718@naver.com>
2022-05-14 11:07:46 +0900

    Initial Commit

---
+++ AutoLayoutExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample
+//
+//  Created by 채명정 on 2022/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
