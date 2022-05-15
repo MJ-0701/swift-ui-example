e2750fbf73b26c146693e7437c566067c3df3a60
MJ-0701 <jack2718@naver.com>
2022-05-15 14:56:09 +0900

    feat(quote): 명언 생성기 실습

---
+++ AutoLayoutExample/Quote.swift
@@ -0,0 +1,15 @@
+//
+//  Quote.swift
+//  AutoLayoutExample
+//
+//  Created by 채명정 on 2022/05/15.
+//
+
+import Foundation
+
+struct Quote{
+    let contents : String
+    let name : String
+    
+    
+}
