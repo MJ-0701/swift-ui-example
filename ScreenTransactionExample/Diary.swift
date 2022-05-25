92e6d6ab0028328f642bb0a894af2ce1e4d6b1bb
MJ-0701 <jack2718@naver.com>
2022-05-25 17:01:31 +0900

    feat(diary): Diary List

---
+++ ScreenTransactionExample/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  ScreenTransactionExample
+//
+//  Created by 채명정 on 2022/05/25.
+//
+
+import Foundation
+
+struct Diary {
+    var title : String
+    var contents : String
+    var date : Date
+    var isStar : Bool
+    
+}
